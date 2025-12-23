285443704d1805a2b4c5f9727f64603644d4b286
Artem <st142163@student.spbu.ru>
2025-12-23 22:36:27 +0300

    creating an abstract class

---
+++ image.h
@@ -0,0 +1,22 @@
+/*
+Epifancev Artem
+st142163@student.spbu.ru
+LabWork1
+*/
+
+#pragma once
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+class Image {
+public:
+    virtual ~Image() = default;
+    
+    virtual bool Load(const std::string& filename) = 0;
+    virtual bool Save(const std::string& filename) = 0;
+    
+    virtual std::size_t Width() const = 0;
+    virtual std::size_t Height() const = 0;
+    virtual std::size_t Memory() const = 0;
+};
